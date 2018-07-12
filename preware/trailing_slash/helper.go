c649c42480ac9a73a5434235db5a94453cc409d7
Matthew Clark <mclark4386@gmail.com>
2018-07-12 16:25:53 -0500

    *fixed root not loading

--- preware/trailing_slash/helper.go
+++ preware/trailing_slash/helper.go
@@ -8,8 +8,8 @@ import (
 // This will check for if the requested path has a trailing slash ('/') and remove it.
 func RemoveTrailingSlash(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		path := strings.ToLower(req.URL.Path)
-		if strings.HasSuffix(path, "/") {
+		path := req.URL.Path
+		if strings.HasSuffix(path, "/") && path != "/" {
 			req.URL.Path = path[:len(path)-1]
 		}
 	})
